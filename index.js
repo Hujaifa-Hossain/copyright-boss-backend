f4c4d7b3adf7fe06b6fb3b49d3cdd9a0e68f8b79
Hujaifa21 <21hujaifa@gmail.com>
2022-09-30 21:16:04 +0600

    worked on file upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const fileUpload = require('express-fileupload');
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -18,15 +19,15 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-function verifyJWT(req, res, next){
+function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message: 'UnAthorized Access'})
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAthorized Access" });
   }
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-    if(err){
-      return res.status(403).send({message: 'Forbidden Access'})
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
     }
     req.decoded = decoded;
     next();
@@ -39,11 +40,9 @@ async function run() {
     const userCollection = client.db("copyright-boss").collection("users");
     const videoCollection = client.db("copyright-boss").collection("videos");
 
-
-    app.get('/', async (req, res) => {
-      res.send('hello')
-    })
-
+    app.get("/", async (req, res) => {
+      res.send("hello from Copyright Boss");
+    });
 
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
@@ -55,40 +54,50 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
-      res.send({result, token});
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
     });
-
 
     app.put("/user/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
       const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({email: requester});
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
 
-      if(requesterAccount.role === 'admin'){
+      if (requesterAccount.role === "admin") {
         const filter = { email: email };
 
         const updateDoc = {
-          $set: {role: 'admin'},
+          $set: { role: "admin" },
         };
         const result = await userCollection.updateOne(filter, updateDoc);
         res.send(result);
-      }else{
-        res.status(403).send({message: 'forbidden'})
+      } else {
+        res.status(403).send({ message: "forbidden" });
       }
     });
 
-
-    app.get('/users', verifyJWT, async(req, res)=>{
-      const users = await userCollection.find().toArray();
-      res.send(users)
-    })
-
-    app.get('/admin/:email', async(req, res)=>{
+    app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
-      const user = await userCollection.findOne({email:email});
-      const isAdmin = user.role === 'admin';
-      res.send({admin: isAdmin});
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    app.get("/users", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.post("/upload", async (req, res) => {
+      const file = req.files.formdata;
+      const result = await videoCollection.insertOne(file);
+      res.send(result);
     })
 
     app.post("/videos", async (req, res) => {
@@ -96,7 +105,6 @@ async function run() {
       const result = await videoCollection.insertOne(video);
       res.send(result);
     });
-
 
     app.get("/videos", async (req, res) => {
       const query = {};
